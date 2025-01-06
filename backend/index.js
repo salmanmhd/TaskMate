c78d97ac3b63ce956c42ee58de27446c927c2166
Md Salman <mhdsalman010@gmail.com>
2025-01-06 19:01:31 +0530

    update error handling in singup and login

--- backend/index.js
+++ backend/index.js
@@ -72,7 +72,9 @@ app.post('/signup', async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
-    res.status(400).json({ message: 'Error creating user' });
+    res
+      .status(400)
+      .json({ message: 'Error creating user', error: error.message });
   }
 });
 
@@ -97,7 +99,7 @@ app.post('/login', async (req, res) => {
     });
     res.json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'Error logging in' });
+    res.status(500).json({ message: 'Error logging in', error: error.message });
   }
 });
 
