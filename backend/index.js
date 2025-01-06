eafa278025ead655ab9b566f632eca7b55bf9aeb
Md Salman <mhdsalman010@gmail.com>
2025-01-06 19:10:04 +0530

    optimize signup and login

--- backend/index.js
+++ backend/index.js
@@ -62,19 +62,20 @@ app.post('/signup', async (req, res) => {
         'Email and password are required. Password must be at least 5 characters long.',
     });
   }
+
   try {
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email }).lean();
     if (existingUser) {
       return res.status(400).json({ message: 'Email is already in use.' });
     }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
-    res
-      .status(400)
-      .json({ message: 'Error creating user', error: error.message });
+    console.error('Error during signup:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
@@ -85,21 +86,25 @@ app.post('/login', async (req, res) => {
       .status(400)
       .json({ message: 'Email and password are required.' });
   }
+
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).lean();
     if (!user) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
+
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
+
     const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: '24h',
     });
     res.json({ token });
   } catch (error) {
-    res.status(500).json({ message: 'Error logging in', error: error.message });
+    console.error('Error during login:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
